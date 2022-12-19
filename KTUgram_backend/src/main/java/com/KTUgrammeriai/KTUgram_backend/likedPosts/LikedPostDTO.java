7cf2a52c1085cc94b979de71cf6af386eda70de2
juskaj <justas.kajokas@ktu.edu>
2022-12-19 11:48:26 +0200

    Update LikedPostDTO.java

--- KTUgram_backend/src/main/java/com/KTUgrammeriai/KTUgram_backend/likedPosts/LikedPostDTO.java
+++ KTUgram_backend/src/main/java/com/KTUgrammeriai/KTUgram_backend/likedPosts/LikedPostDTO.java
@@ -3,14 +3,14 @@ import com.KTUgrammeriai.KTUgram_backend.post.PostDTO;
 import com.KTUgrammeriai.KTUgram_backend.user.UserDTO;
 import lombok.Data;
 
-import java.sql.Time;
+import java.time.LocalTime;
 import java.util.Date;
 
 @Data
 public class LikedPostDTO {
     private long id;
     private Date date;
-    private Time time;
+    private LocalTime time;
     private PostDTO post;
     private UserDTO user;
 }
